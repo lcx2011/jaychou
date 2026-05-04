27bddd00df58595cc1c60809dccf78da83e678b4
lcx2011 <2743566355@qq.com>
2026-05-04 11:57:51 +0800

    调参

--- src/components/tabs/VersusTab.tsx
+++ src/components/tabs/VersusTab.tsx
@@ -5,7 +5,7 @@ import { calculateTotal, calculateTier } from '../../utils/scoring';
 import type { Song, DimensionScores } from '../../types';
 
 const K = 0.3;
-const D_PARAM = 250;
+const D_PARAM = 50;
 
 interface DeltaRecord {
   dimKey: string;
